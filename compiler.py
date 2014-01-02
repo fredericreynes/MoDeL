276dc1e73473488dd31c803e0061b7962249f6aa
othellogit <aurelien.saussay@gmail.com>
2014-01-02 17:39:55 +0100

    Change directory if passed a working path as command line parameter

--- compiler.py
+++ compiler.py
@@ -9,6 +9,9 @@ import ntpath
 import csv
 import os
 
+if len(sys.argv) > 1:
+    os.chdir(sys.argv[1])
+
 with open('tmp_all_vars.csv', 'rb') as csvfile:
     rows = list(csv.reader(csvfile))
     heap = dict(zip(rows[0],
