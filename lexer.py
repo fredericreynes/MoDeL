d7213a9b394498cbb4aa2bc2237f5a1bd1deb843
Aurelien Saussay <aurelien.saussay@gmail.com>
2014-04-22 17:06:19 +0200

    Lexer scans operators, names, parens

--- lexer.py
+++ lexer.py
@@ -3,18 +3,39 @@ from StringIO import StringIO
 
 class Lexer(Scanner):
     digit = Range("09")
-    int = Rep1(digit)
-    real = Rep1(digit) + Str('.') + Rep1(digit)
+    integer = Opt(Str('-')) + Rep1(digit)
+    real = Opt(Str('-')) + Rep1(digit) + Str('.') + Rep1(digit)
+    operators = Any("+-*/")
+    alpha = NoCase(Range("az"))
+    name = Rep1(Any("@_") | alpha | digit)
+    local_name = Str("%") + name
 
+    space = Any(" \t\n")
 
     lexicon = Lexicon([
-        (int, 'int'),
+        (integer, 'integer'),
         (real, 'real'),
+        (operators, 'operator'),
+        (name, 'name'),
+        (local_name, 'local'),
+        (Str('('), 'lparen'),
+        (Str(')'), 'rparen'),
+        (Str('['), 'lbracket'),
+        (Str(']'), 'rbracket'),
+        (Str('{'), 'lcurly'),
+        (Str('}'), 'rcurly'),
+        (space, IGNORE)
     ])
+
+    def read_all(self):
+        ret = []
+        token = self.read()
+        while not (token[0] is None):
+            ret.append(token)
+            token = self.read()
+        return ret
 
     def __init__(self, file):
         Scanner.__init__(self, self.lexicon, file)
 
-
-lexer = Lexer(StringIO("50"))
-print lexer.read()
+# print Lexer(StringIO("PRF[sm]{-1}")).read_all()
--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -5,8 +5,47 @@ class TestLexer:
     def _test(self, input, output):
         assert lexer.Lexer(StringIO(input)).read() == output
 
+    def _test_all(self, input, output):
+        assert lexer.Lexer(StringIO(input)).read_all() == output
+
     def test_scans_integer(self):
-        self._test("42", ('int', '42'))
+        self._test("42", ('integer', '42'))
+        self._test("-42", ('integer', '-42'))
 
     def test_scans_real(self):
         self._test("3.14159", ('real', '3.14159'))
+
+    def test_scans_operator(self):
+        self._test("+", ('operator', '+'))
+
+    def test_scans_arithmetic_expression(self):
+        self._test_all("42 + 3.14159 / 1981",
+                       [('integer', '42'),
+                        ('operator', '+'),
+                        ('real', '3.14159'),
+                        ('operator', '/'),
+                        ('integer', '1981')])
+
+    def test_scans_names(self):
+        self._test("P", ('name', 'P'))
+        self._test("EFER_n", ('name', 'EFER_n'))
+        self._test("@elem", ('name', '@elem'))
+        self._test("_test", ('name', '_test'))
+
+    def test_local_names(self):
+        self._test("%list_com", ('local', '%list_com'))
+
+    def test_scans_identifiers(self):
+        self._test_all("P[c]",
+                       [('name', 'P'),
+                        ('lbracket', '['),
+                        ('name', 'c'),
+                        ('rbracket', ']')])
+        self._test_all("PRF[sm]{-1}",
+                       [('name', 'PRF'),
+                        ('lbracket', '['),
+                        ('name', 'sm'),
+                        ('rbracket', ']'),
+                        ('lcurly', '{'),
+                        ('integer', '-1'),
+                        ('rcurly', '}')])
